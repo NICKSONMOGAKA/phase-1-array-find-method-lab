998a7a549d1cd8c3cba69794cfed5d4b063132f5
+ <nickson.mogaka@student.moringaschool.com>
2024-07-02 16:25:59 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+exports.findMatching = function(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  };
+  
+  exports.fuzzyMatch = function(drivers, letters) {
+    return drivers.find(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  };
+  
+  exports.matchName = function(drivers, name) {
+    return drivers.includes(name);
+  };
+  
+  exports.superbowlWin = function(record) {
+    const wins = [1997, 1998, 2015];
+    
+    for (let i = 0; i < wins.length; i++) {
+      if (record.includes(wins[i])) {
+        return wins[i];
+      }
+    }
+    
+    return undefined;
+  };
+  
